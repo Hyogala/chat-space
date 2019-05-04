89120a58951fd7a0ad9f3eb4f748a26f169409e6
sakuma <skm98765@gmail.com>
2019-05-04 15:21:10 +0900

    MarkUp form
    ほか細々とした修正など

--- app/controllers/messages_controller_controller.rb
+++ app/controllers/messages_controller_controller.rb
@@ -1,5 +1,14 @@
 class MessagesControllerController < ApplicationController
 
   def index
+    # @message = Message.new
   end
+  # def show
+  #   @message = Message.new
+  # end
+  # def create
+  #   @message = Message.new(params[:message])
+  #   @message.save
+  #   # redirect_to new_user_path
+  # end
 end

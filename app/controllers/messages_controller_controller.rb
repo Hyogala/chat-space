8255684e7f7bb628080f9f7ee4f033769a78a57d
sakuma <skm98765@gmail.com>
2019-05-05 16:47:52 +0900

    MarkUp Main View #2
    レビュー対応
    ・link_to
    ・コメントアウト削除
    ・&

--- app/controllers/messages_controller_controller.rb
+++ app/controllers/messages_controller_controller.rb
@@ -1,14 +1,5 @@
 class MessagesControllerController < ApplicationController
 
   def index
-    # @message = Message.new
   end
-  # def show
-  #   @message = Message.new
-  # end
-  # def create
-  #   @message = Message.new(params[:message])
-  #   @message.save
-  #   # redirect_to new_user_path
-  # end
 end
